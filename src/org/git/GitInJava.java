ec16237e010ea7bde54275987ec497235a83543f
ManikandanPrabuS <manikandanprabu2912@gmail.com>
2022-09-11 21:23:14 +0530

    firstgit

---
+++ src/org/git/GitInJava.java
@@ -0,0 +1,39 @@
+package org.git;
+
+public class GitInJava {
+	
+	private void add() {
+		short year= 1998;
+		byte age=24;
+		int pincode=600081;
+		long phno= 7871739467l;
+		float salary=58563.89765f;
+		double price= 25823.1356;
+		String name="Manikandan prabu";
+		char gender='M';
+		Boolean verkadala=false;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(year);
+		System.out.println(pincode);
+		System.out.println(phno);
+		System.out.println(salary);
+		System.out.println(gender);
+		System.out.println(price);
+		System.out.println(verkadala);
+		
+		
+
+	}
+
+
+	
+	public static void main(String[] args) {
+		GitInJava d= new GitInJava();
+		d.add();
+		
+
+	}
+
+}
